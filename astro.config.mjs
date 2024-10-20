143761a36605e9071edbe44efd0af451628f5f14
k-imoto <git@k-i-style.com>
2024-10-20 13:02:35 +0900

    fix: sass関連の警告解消

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
 });
